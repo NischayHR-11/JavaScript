3f8eb6f7b372a1eaad1e962df342fe2413d50518
NISCHAY HR <nischayhr11@gmail.com>
2024-07-10 00:18:23 +0530

    Try & Ctach Method IS Implemented And Arrow function Method is also Implemented

--- ErrorHandling/Try&Catch.js
+++ ErrorHandling/Try&Catch.js
@@ -15,3 +15,31 @@ student = {
 
 student.TotalCgpa();
 
+
+
+console.log("HI");
+student.TotalCgpa();
+try{
+    console.log(a);
+}catch(er){
+
+    console.log("ERROR DETECTED !!, UNDEFINED VARIABLE..");
+}
+
+console.log("BYE....");
+
+
+// Arrow Function
+
+const sum= (a,b)=>{
+
+    console.log("SUM IS : "+(a+b));
+}
+
+sum(2,5);
+
+// Implicit Return in Arrow Function [ only variable name defines the return  , here we use () instead Of {}]
+
+const mul= (a,b) =>(a*b);
+
+console.log("MULTIPLICATION IS : "+ mul(5,4) );
