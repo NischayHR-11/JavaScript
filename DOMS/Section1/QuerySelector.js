6b2ce7f2797bcf795b2f985cd61e7932125421e9
NISCHAY HR <nischayhr11@gmail.com>
2024-08-16 01:09:29 +0530

    Added js File with Query Selector Concept And others

---
+++ DOMS/Section1/QuerySelector.js
@@ -0,0 +1,7 @@
+console.dir(document.getElementById("description"));
+console.dir(document.getElementsByClassName("images"));
+console.dir(document.getElementsByTagName("img"));
+
+console.dir(document.querySelectorAll("img"));
+console.dir(document.querySelector("p"));
+
