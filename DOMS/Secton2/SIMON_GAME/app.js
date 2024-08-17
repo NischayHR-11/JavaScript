e009b34e890ecea2996918ed9ab74c912babe1df
NISCHAY HR <nischayhr11@gmail.com>
2024-08-17 23:59:28 +0530

    JS File Of The simon Game Is Added

---
+++ DOMS/Secton2/SIMON_GAME/app.js
@@ -0,0 +1,86 @@
+let but=document.querySelectorAll(".box");
+let h2=document.querySelector("h2");
+let level=0;
+let start=false;
+
+let color=["blue","yellow","red","purple"];
+let game=[];
+let user=[];
+
+function flash(btn){
+
+    btn.classList.add("flash");
+    setTimeout(function(){
+
+        btn.classList.remove("flash");
+    },100);
+
+}
+
+function userflash(btn){
+
+    btn.classList.add("green");
+    setTimeout(function(){
+
+        btn.classList.remove("green");
+    },100);
+
+}
+
+function levelup(){
+
+    user=[];
+    level++;
+    h2.innerText="LEVEL "+level; 
+
+    let r=Math.floor(Math.random()*3);
+    let clr=color[r];
+    let rbt=document.querySelector("."+clr);
+    game.push(clr);
+    flash(rbt);
+}
+
+document.addEventListener("keypress",function(){
+
+    if(start==false){
+    start=true;
+    levelup();
+    }
+});
+
+function reset(){
+
+    start=false;
+    user=[];
+    game=[];
+    level=0;
+}
+
+function check( ind){
+
+    console.log("level = "+level);
+    if(user[ind]===game[ind]){
+
+        if(user.length==game.length){
+            levelup();
+        }
+
+    }else{
+
+        h2.innerText="GAME OVER !! Your Score Is : "+level+"  Press Any Key To Play Again";
+        reset();
+    }
+}
+
+
+for(button of but){
+
+    button.addEventListener("click",function(){
+
+        userflash(this);
+        console.log(this.getAttribute("id"));
+        user.push(this.getAttribute("id"));
+
+        check(user.length-1);
+    })
+}
